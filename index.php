8aca547412697b3537bb7c7bcd8b51fd18f7acf0
Aleksandr Kustov <a.n.kustov@gmail.com>
2014-04-30 23:17:42 -0700

    file structure

--- index.php
+++ index.php
@@ -1,116 +1,50 @@
 <?php
-require_once './config.php';
-require_once './medoo.min.php';
+require_once './header.php';
 
-$db = new medoo(array(
-    'database_type' => 'mysql',
-    'database_name' => Config::dbname,
-    'server' => Config::dbhost,
-    'username' => Config::dbuser,
-    'password' => Config::dbpassword,
-    'port' => 3306,
-    'charset' => 'utf8'
-        ));
+if (isset($_POST['new_note']) && trim($_POST['new_note']) !== "") {
+    $db->insert("notes", array(
+        "text" => $_POST['new_note']
+    ));
+    header("Location: " . $_SERVER['REQUEST_URI']);
+    exit();
+}
 ?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <style>
-            body {
-                padding-top: 50px;
-                padding-bottom: 20px;
-            }
-        </style>
+<body>
+    <!--[if lt IE 7]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php
+    require_once './menu.php';
+    ?>
 
-        <link rel="stylesheet" href="css/main.css">
-
-        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Ez-notes</a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <form class="navbar-form navbar-right" role="form">
-                        <div class="form-group">
-                            <input type="text" placeholder="Email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Password" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-success">Sign in</button>
-                    </form>
-                </div><!--/.navbar-collapse -->
-            </div>
-        </div>
-
-        <?php $notes = $db->select("notes", "*");
+    <div class="container main-container">
+        <?php
+        $notes = $db->select("notes", "*");
         foreach ($notes as $note) {
-            ?>
-            <div class="container main-container">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <?php echo $note['time']; ?>
-                        <button type="button" class="close" aria-hidden="true">&times;</button>
-                    </div>
-                    <div class="panel-body"><?php echo $note['text']; ?>
-                    </div>
+            ?>    
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <?php echo $note['time']; ?>
+                    <button type="button" class="close" aria-hidden="true">&times;</button>
                 </div>
-            <?php } ?>
-
-            <form  role="form">
-                <div class="form-group">
-                    <textarea class="form-control"></textarea>
+                <div class="panel-body"><?php echo htmlspecialchars($note['text']); ?>
                 </div>
-                <div class="form-group">
-                    <button type="button" class="btn btn-default pull-right">Submit</button>
-                </div>
-            </form>
+            </div>
+        <?php } ?>
 
-        </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <form  role="form" method="post">
+            <div class="form-group">
+                <textarea class="form-control" name="new_note"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-default pull-right" value="Submit"/>
+            </div>
+        </form>
 
-        <script src="js/vendor/bootstrap.min.js"></script>
-
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b, o, i, l, e, r) {
-                b.GoogleAnalyticsObject = l;
-                b[l] || (b[l] =
-                        function() {
-                            (b[l].q = b[l].q || []).push(arguments)
-                        });
-                b[l].l = +new Date;
-                e = o.createElement(i);
-                r = o.getElementsByTagName(i)[0];
-                e.src = '//www.google-analytics.com/analytics.js';
-                r.parentNode.insertBefore(e, r)
-            }(window, document, 'script', 'ga'));
-            ga('create', 'UA-XXXXX-X');
-            ga('send', 'pageview');
-        </script>
-    </body>
+    </div> <!-- /container -->        
+    <?php
+    require_once './footer.php';
+    ?>
+</body>
 </html>
---
+++ user.php
@@ -0,0 +1,19 @@
+<?php
+
+class user {
+    
+   private $userId;
+   private $db;
+   
+   public function __construct($db){
+       $this->db = $db;
+   }
+   
+   public function login($email, $password){
+       
+   }
+   
+   public function isLogged(){
+       
+   }
+}
